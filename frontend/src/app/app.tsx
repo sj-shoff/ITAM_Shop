8479edf08965c8fc8ab510b011ebef32608bfc56
Artemitol <artemiy.averin@gmail.com>
2024-12-01 00:03:49 +0300

    добавил поддержку redux-toolkit

--- frontend/src/app/app.tsx
+++ frontend/src/app/app.tsx
@@ -2,7 +2,13 @@ import { RouterProvider } from "react-router-dom"
 
 import "./main.scss"
 import { router } from "./router/router"
+import { Provider } from "react-redux"
+import { store } from "./storage"
 
 export function App() {
-    return <RouterProvider router={router} />
+    return (
+        <Provider store={store}>
+            <RouterProvider router={router} />
+        </Provider>
+    )
 }
---
+++ frontend/src/app/storage/storage.ts
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/toolkit"
+
+const reducer = (state, action) => {}
+
+export const store = configureStore({
+    reducer: reducer,
+})
